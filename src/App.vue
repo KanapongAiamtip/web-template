93c502698b27795dfd06bed133b2b23beed5155a
Louis Thanawin <kannaphongi62@nu.ac.th>
2021-10-13 17:35:34 +0700

    App

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <router-view></router-view>
+</template>
+<script lang="ts" src="./app.ts"></script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: hsla(171, 68%, 66%, 0.988);
+  margin-top: 60px;
+}
+</style>
